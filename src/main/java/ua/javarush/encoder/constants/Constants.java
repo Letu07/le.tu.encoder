9b74d330ab8991ee6006788de52af0fe44f7628c
Letu <lean7ua@gmail.com>
2023-08-23 18:21:55 +0300

    done Constants class

--- src/main/java/ua/javarush/encoder/constants/Constants.java
+++ src/main/java/ua/javarush/encoder/constants/Constants.java
@@ -1,5 +1,21 @@
 package ua.javarush.encoder.constants;
 
-public class Constants {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Constants {
+    public static final ArrayList<Character> ALPHABET_EN = new ArrayList<>(
+            Arrays.asList(
+                    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+                    )
+    );
+
+    public static final ArrayList<Character> ALPHABET_UA = new ArrayList<>(
+            Arrays.asList(
+                    'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї',
+                    'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
+                    'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'
+            )
+    );
 }
